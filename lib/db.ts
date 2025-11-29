81c0b1991d9d97107b83b676e0782ee0a2a6566f
Muskan Kumari <muskangen1395@gmail.com>
2025-11-29 14:16:23 +0530

    #

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client"
-
+// imported prisma
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient
@@ -16,5 +16,6 @@ export const prisma = globalForPrisma.prisma || new PrismaClient({
 
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma 
+
 
 // export const prisma = new PrismaClient()
