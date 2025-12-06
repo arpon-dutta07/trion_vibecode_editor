5e2bbdf559260adcc159b92aa927ecff598a163a
Arpon Dutta <arpongen3094@gmail.com>
2025-12-06 14:28:00 +0530

    feat(auth): integrate sign-in form component into page
    Add SignInFromClient component to the sign-in page layout, update logo image source and dimensions, and apply responsive styling with flexbox for centered alignment. This enhances the authentication flow by providing an interactive form alongside the visual elements.

--- app/(auth)/auth/sign-in/page.tsx
+++ app/(auth)/auth/sign-in/page.tsx
@@ -1,16 +1,20 @@
 
+import SignInFromClient from '@/features/auth/components/signin-form-client'
 import Image from 'next/image'
 import React from 'react'
 
 const SignInPage = () => {
   return (
     <>
-        <Image src={"/login.svg"} alt="Login-Image" height={300} 
-        width={300}
+      <div className='space-y-6 flex flex-col items-center justify-center'>
+        <Image src={"/logo.png"} alt="Login-Image" height={400} width={400}
         className='m-6 object-cover'
         />
+        <SignInFromClient />
+        </div>
     </>
   )
 }
 
 export default SignInPage
+
